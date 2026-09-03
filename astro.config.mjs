8ef5d2de0d4db684ead5d8743d8c7ca837f634d2
Jacob Feldman <jacobfeldman@openrules.com>
2026-09-03 07:49:47 -0400

    Serve DecisionCAMP from custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://decision-management-community.github.io',
-  base: '/decisioncamp2027',
+  site: 'https://decisioncamp.org',
 });
